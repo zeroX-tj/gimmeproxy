4abf7eb9a6feadc9beb5cfe9c9131c1ae7a03afc
Tim Janssens <tim@emilioit.com>
2016-05-17 11:31:50 +0200

    make it possible to request type, disable timeouts

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ var request = require('request-promise')
 var GimmeProxy = {
   '_userId': null,
   '_timeout': null,
+  '_type': null,
   '_host': 'http://gimmeproxy.com',
   '_path': '/api/getProxy',
   '_timeout_path': '/api/get',
   'config': function(config) {
     if (config.userId) this._userId = config.userId;
     if (config.timeout) this._timeout = config.timeout;
-
+    if (config.type) this._type = config.type;
     // if there is no user id get a random one so timeouts can still work
     if (! this._userId) {
       this._userId = randomstring.generate(32);
@@ -23,10 +24,9 @@ var GimmeProxy = {
     // we are gonna need a user id
     if (timeout) this.config({});
 
-    var path = timeout ? (this._timeout_path + '/' + this._userId + '/?timeout=' + timeout) : this._path;
-    var url = this._host + path;
-
-
+    var path = timeout ? (this._timeout_path + '/' + this._userId + '/?timeout=' + timeout) : this._path; // API gives 404 on timeouts
+    var url = this._host + this._path;
+    if(this._type) url += '?type='+this._type;
 
     return request(url).then(function(resp) {
       return JSON.parse(resp);
